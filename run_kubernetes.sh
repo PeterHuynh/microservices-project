f1144dbc599737a1678e068de59c4e3c191dbef3
Peter Huynh <peter.huynh99@gmail.com>
2023-01-31 11:17:21 -0700

    add run kubernetes and upload docker scripts.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,28 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+# dockerpath=sklearn-project-app:v1.0.0
+deployment_name="microservice-project-app"
+app_name="microservices"
+app_version="v1.0.0"
+docker_id="peterhuynh99"
+dockerpath="${docker_id}/${app_name}:${app_version}"
+echo "image path: ${dockerpath}"
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ${deployment_name} --image=${dockerpath} --port=80 --labels app=${app_name}
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
+sleep 30
+pod_name=$(kubectl get pods | grep ${deployment_name} | awk '{print $1}')
+echo "pod name: ${pod_name}"
+echo "Forwarding port..."
+sleep 30
+kubectl get pods 
+kubectl port-forward pod/${pod_name} --address 0.0.0.0 8000:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=peterhuynh99/microservices
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag sklearn-project-app peterhuynh99/microservices:v1.0.0
 
 # Step 3:
 # Push image to a docker repository
+docker push peterhuynh99/microservices:v1.0.0
